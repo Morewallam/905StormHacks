cb9815139864dcc9edd92e350153937e7fbd469f
Sean Wallace <swallace987@gmail.com>
2025-03-08 22:31:12 -0800

    Back end for getting single and all cards good

--- insanity_academy/server/main.py
+++ insanity_academy/server/main.py
@@ -1,40 +1,77 @@
-from typing import Union
+from contextlib import asynccontextmanager
+from typing import Annotated, Optional, Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
-
-app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
-class Card(BaseModel):
-    id: int
+class Card(SQLModel, table=True):
+    card_id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(index=True)
     front: str
     back: str
+    times_correct: int
+    times_tried: int
+    mad: int | None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class User(BaseModel):
+    user_id: int
+    name: str
+    madness: int | None
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+sqlite_file_name = "database/database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
-@app.get("/cards")
-def get_cards():
-    # Query the database to get the cards
+def get_session():
+    with Session(engine) as session:
+        yield session
 
-    return {"cards"}
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/cards")
+def create_card(card: Card, session: SessionDep) -> Card:
+    session.add(card)
+    session.commit()
+    session.refresh(card)
+    return card
+
+
+@app.get("/cards")  # Get all the cards
+def get_all_cards(session: SessionDep) -> list[Card]:
+    cards = session.exec(select(Card)).all()
+    return cards
+
+
+@app.get("/cards/{card_id}")  # Get all the cards
+def get_card(card_id: int, session: SessionDep):
+    card = session.get(Card, card_id)
+    if not card:
+        raise HTTPException(status_code=404, detail="Card not found")
+    return card
+
+
+@app.put("/cards/update/{card_id}")
+def update_card(card_id: int, correct: bool | None):
+    # Update database with card and correct
+    return {}
